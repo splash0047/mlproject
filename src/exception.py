ce0bf2f55f7136a389826b290548cc6fd15a386a
splash0047 <your-email@example.com>
2025-07-12 10:58:03 +0530

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_details(error: Exception, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -10,12 +10,11 @@ def error_message_details(error: Exception, error_detail: sys):
     return error_message
         
     
-    class CustomException(Exception):
-        def __init__(self, error_message, error_detail):
-            super().__init__(error_message)
-            self.error_message = error_message_details(error_message, error_detail=error_detail)
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail=error_detail)
     
-        def __str__(self):
-            return self.error_message
-    
-   
+    def __str__(self):
+        return self.error_message
+
